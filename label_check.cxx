b33f61807e9e804b61b1b3ce195030697909f5d9
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-04-20 17:51:19 +0200

    add skeleton of the label filter
    darcs-hash:20070420155119-2fc9d-cf6ef5e91abf9246b60e37f2f641040cd0789723.gz

---
+++ label_check.cxx
@@ -0,0 +1,38 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkLabelPerimeterEstimationImageFilter.h"
+
+
+int main(int argc, char * argv[])
+{
+
+  if( argc != 2 )
+    {
+    std::cerr << "usage: " << argv[0] << " " << std::endl;
+    // std::cerr << "  : " << std::endl;
+    exit(1);
+    }
+
+  const int dim = 2;
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::LabelPerimeterEstimationImageFilter< IType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+
+  itk::SimpleFilterWatcher watcher(filter, "filter");
+
+  filter->Update();
+
+  return 0;
+}
+
